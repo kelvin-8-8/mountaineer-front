cdcad20e3ec6bd82079a428402bc46b3dd7f1257
kelvin.win <kelvin.tung@hotmail.com>
2024-12-10 12:30:30 +0800

    footer improve

--- src/components/ComponentTesting.jsx
+++ src/components/ComponentTesting.jsx
@@ -4,105 +4,72 @@ import SwitchButton from "../components/SwitchButton";
 import UserButton from "../components/UserButton";
 
 export default function ComponentTesting() {
+  const goToTop = () => {
+    console.log("Scroll to top function called");
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
-    <div className="flex flex-col justify-center items-center bg-base-200">
-      <div className="navbar bg-base-200 max-w-screen-xl z-50 relative">
-        {/* 左側 */}
-        <div className="navbar-start">
-          {/* 手機板 下拉式 */}
-          <div className="dropdown">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle lg:hidden"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </div>
-            <ul
-              tabIndex={0}
-              className="relative top-10 menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-            >
-              {/* HOME */}
-              <li>
-                <a>Home</a>
-              </li>
-              {/* ABOUT */}
-              <li>
-                <a>About</a>
-              </li>
-              {/* Features */}
-              <li>
-                <a>Features</a>
-                <ul className="p-2">
-                  <li>
-                    <a>借裝備</a>
-                  </li>
-                  <li>
-                    <a>隊伍資訊</a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </div>
-          {/* 標題 */}
-          <a className="btn btn-ghost text-xl">daisyUI</a>
-        </div>
+    <footer className="footer bg-base-200 items-center p-4">
+      <aside className="grid-flow-col items-center">
+        <p>
+          Copyright © {new Date().getFullYear()} Montainner™- All right reserved
+        </p>
+      </aside>
 
-        {/* 中間 */}
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            {/* HOME */}
-            <li>
-              <a>Home</a>
-            </li>
-            {/* ABOUT */}
-            <li>
-              <a>About</a>
-            </li>
-            {/* Features */}
-            <li>
-              <details>
-                <summary>Features</summary>
-                <ul className="relative top-6 w-32">
-                  <li>
-                    <a>借裝備</a>
-                  </li>
-                  <li>
-                    <a>隊伍資訊</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-          </ul>
-        </div>
-        {/* 右邊 */}
-        <div className="navbar-end">
-          <ul className="flex flex-row">
-            <li>
-              <Link to="/login">
-                <UserButton />
-              </Link>
-            </li>
+      <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+        {/* Back to top button */}
+        <label onClick={goToTop} className="btn btn-ghost btn-circle hover:cursor-pointer">
+          <svg
+            fill="#000000"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            viewBox="0 0 330 330"
+            xml:space="preserve"
+            className="fill-current w-6 h-6"
+          >
+            <path
+              id="XMLID_224_"
+              d="M325.606,229.393l-150.004-150C172.79,76.58,168.974,75,164.996,75c-3.979,0-7.794,1.581-10.607,4.394 l-149.996,150c-5.858,5.858-5.858,15.355,0,21.213c5.857,5.857,15.355,5.858,21.213,0l139.39-139.393l139.397,139.393 C307.322,253.536,311.161,255,315,255c3.839,0,7.678-1.464,10.607-4.394C331.464,244.748,331.464,235.251,325.606,229.393z"
+            />
+          </svg>
+        </label>
+        {/* <a>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            className="fill-current"
+          >
+            <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+          </svg>
+        </a>
+        <a>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            className="fill-current"
+          >
+            <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+          </svg>
+        </a>
+        <a>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            className="fill-current"
+          >
+            <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+          </svg>
+        </a> */}
 
-            <li>
-              <SwitchButton />
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+      </nav>
+    </footer>
   );
 }
